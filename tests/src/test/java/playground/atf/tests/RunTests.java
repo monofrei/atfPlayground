52b7acb5cdd174b8e9d05f54b72b51b8644082f6
Marin Onofrei <marin.onofrei@endava.com>
2017-07-20 18:20:45 +0300

    runner

--- tests/src/test/java/playground/atf/tests/RunTests.java
+++ tests/src/test/java/playground/atf/tests/RunTests.java
@@ -9,13 +9,14 @@ import playground.atf.tests.uiTests.CustomCucumberRunner;
 /**
  * Created by cpascal on 3/27/2017.
  */
-//@RunWith(CustomCucumberRunner.class)
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
         glue = {"playground"},
         features ={"src/test/resources/"},
-        plugin = {"pretty","html:target/cucumber/"}//"playground.atf.tests.ELKFormatter"}
+       format = {"json:target/cucumber.json"}
+
 )//
-//@ContextConfiguration("classpath:cucumber.xml")
+
 public class RunTests {
 }
